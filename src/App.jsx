a6e6c05129a649f87ea9e288d62a902b8c00e5f2
1mr-newton <eaidoo015@gmail.com>
2023-08-31 15:06:37 +0000

    created project with base layouts

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./components/Home";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+export default function Home() {
+  return (
+    <main id="main">
+      <div id="container">
+        <Navbar />
+        <div id="profiles-container"></div>
+      </div>
+      <div id="background"></div>
+    </main>
+  );
+}
